50d09c28f82d4e9974e778f55db9599b7b5b1266
RodrigoSpano <spanorodri@gmail.com>
2025-09-09 21:49:27 -0300

    connected Postgres DB

--- src/postgresql/postgresql.module.ts
+++ src/postgresql/postgresql.module.ts
@@ -8,11 +8,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        host: configService.get<string>('postgres.host', '127.0.0.1'),
+        host: configService.get<string>('postgres.host'),
         database: configService.get<string>('postgres.dbname'),
-        port: configService.get<number>('postgres.port', 27017),
+        port: configService.get<number>('postgres.port'),
         type: 'postgres',
-        userName: configService.get<string>('postgres.username'),
+        username: configService.get<string>('postgres.username'),
         password: configService.get<string>('postgres.password'),
         synchronize: true,
       }),
